499d5368aba554cc6b1236d4313dd3b86f987fea
Warner Losh <imp@freebsd.org>
2018-04-15 14:26:30 -0600

    Start a relocation framework.

---
+++ tools/ddb/db_reloc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/param.h>
+
+#include <ddb/ddb.h>
+#include <ddb/db_sym.h>
+#include <ddb/db_reloc.h>
+
+#ifndef MAXNORELOCTABS
+#define  MAXNORELOCTABS	2	/* text, data */
+#endif
+static db_reloctab_t	db_reloctabs[MAXNORELOCTABS] = { { 0, }, };
+static int		db_nreloctab = 0;
+
+void
+db_add_reloc_table(char *start, char *end, char *name, char *extra)
+{
+	if (db_nreloctab >= MAXNORELOCTABS) {
+		printf ("No slots left for %s relocation table", name);
+//		panic ("db_sym.c: db_add_reloc_table");
+		exit(1);
+	}
+
+	db_reloctabs[db_nreloctab].start = start;
+	db_reloctabs[db_nreloctab].end = end;
+	db_reloctabs[db_nreloctab].name = name;
+	db_reloctabs[db_nreloctab].private = extra;
+	db_nreloctab++;
+}
+
+bool
+db_printreloc(db_expr_t offset, db_strategy_t strategy, db_expr_t loc)
+{
+//	return X_db_printrel(offset, strategy, loc);
+	printf("\n looking up loc %d offset %d strat %d\n", loc, offset, strategy);
+	return true;
+}
+
---
+++ tools/ddb/db_reloc.h
@@ -0,0 +1,17 @@
+#ifndef _DDB_DB_RELOC_H
+#define _DDB_DB_RELOC_H
+
+/*
+ * This module can handle multiple symbol tables
+ */
+typedef struct {
+	char		*name;		/* symtab name */
+	char		*start;		/* symtab location */
+	char		*end;
+	char		*private;	/* optional machdep pointer */
+} db_reloctab_t;
+
+void	db_add_reloc_table(char *, char *, char *, char *);
+bool	db_printreloc(db_expr_t, db_strategy_t, db_expr_t);
+
+#endif /* _DDB_DB_RELOC_H */
