db7261812277f2aa6d8951ee2a32a4dd8eecd5bd
Warner Losh <imp@freebsd.org>
2020-04-19 08:54:24 -0600

    First attempt to reconstruct crt0.s
    This assembles to nearly the same as the original crt0.o. However, there's some
    differences in the relocation elements, for some reason. dis88 gives the same
    output of the two different .o's, but hexdump shows differences in the reloc
    section for reasons unknown.
    
    Note: this is with the VENIX /bin/as running under my hacked up 86sim+venix
    system calls thing also in this repo. For the two or three trivial test cases I
    have, running like this produces identical output to the as running on the
    Rainbow, so any differences at all need to be investigated when I have some
    time.

---
+++ src/libc/csu/crt0.s
@@ -0,0 +1,56 @@
+	| C runtime startup
+	| Reconstructed from v7 sources and disassembled crt0.o
+
+	.globl	start
+	.globl	_environ,_main,_exit
+
+	.text
+
+start:
+	|
+	| Initialize the FPU. int $f4 is the vector that Venix uses
+	| to run real FP code on machines w/o 8087 hardware.
+	|
+	int	0xf4
+	fldcw	L001
+	mov	bp,sp
+	mov	si,bp
+
+	|
+	| Find where envp starts. Save it globally as well as passing it to
+	| main.
+	|
+L000:
+	add	si,*2
+	cmp	(si),*0
+	jnz	L000
+	add	si,*2
+	push	si
+	mov	_environ,si
+
+	|
+	| Compute where argv starts on the stack
+	|
+	mov	ax,bp
+	add	ax,#2
+	push	ax
+
+	|
+	| Push argc here
+	|
+	mov	ax,*0(bp)
+	push	ax
+	call	_main
+	add	sp,*6
+	push	ax
+	call	_exit		| Libc's exit
+	pop	cx
+	mov	bx,#1		| and then try the system call if that didn't work.
+	int	0xf1
+
+	.data
+	.byte	0,0		| v7 has these two bytes here for a NULL pointer...
+L001:
+	.byte	0xbf,0x0f
+	.comm	_environ,2
+
