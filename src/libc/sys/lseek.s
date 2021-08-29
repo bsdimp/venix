e5b5e7c7e5e674bb1799c8d25cb3d99fa5d35af0
M. Warner Losh <Warner Losh>
2021-08-29 15:32:55 -0600

    revert lseek, it can't be easily generated

--- src/libc/sys/lseek.s
+++ src/libc/sys/lseek.s
@@ -1,15 +1,22 @@
 	.comm	_errno,2
 	.globl	_errno
 	.globl	_lseek
+	|
+	| Normally, we'd be able to generate this and get an exact
+	| match. However, the push si and the mov bp,sp are swapped.
+	| This makes all the other offsets off by two because of it.
+	| It's not right stylistically, but is no faster or slower
+	| than the stylistic code. Both produce the same results.
+	|
 _lseek:
 	push	bp
-	mov	bp,sp
 	push	si
+	mov	bp,sp
 	mov	bx,#19
-	mov	ax,*4(bp)
-	mov	dx,*6(bp)
-	mov	cx,*8(bp)
-	mov	si,*10(bp)
+	mov	ax,*6(bp)
+	mov	dx,*8(bp)
+	mov	cx,*10(bp)
+	mov	si,*12(bp)
 	int	0xf1
 	jcxz	L001
 	mov	_errno,cx
